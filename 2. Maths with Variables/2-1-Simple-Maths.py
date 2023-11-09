d0ecac4bfd714a05c89cdb9e44487f3934ac638c
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-11-09 06:59:25 +0000

    Create 2-1-Simple-Maths.py

---
+++ 2. Maths with Variables/2-1-Simple-Maths.py
@@ -0,0 +1,19 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1 - Add comments to this code to predict what it will do.
+print(8 + 2)
+#10
+print( 8 - 2)
+
+print(8 * 2)
+
+print(8 / 2)
+
+print(8 // 2)
+
+#LC
+#print(8 % 2)
+
+#print (8 ** 2)
+
+# Task 2 - Write code that calculates and outputs the following numbers: 2001, 1024, 42, 15#
