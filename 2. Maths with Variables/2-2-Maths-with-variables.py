f403c7c7353749927bb956ad6a2e420a26337e73
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-11-09 07:00:16 +0000

    Create 2-2-Maths-with-variables.py

---
+++ 2. Maths with Variables/2-2-Maths-with-variables.py
@@ -0,0 +1,12 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1 - Add comments to predict what the code below will do.
+
+num1 = 20
+num2 = 5
+
+result = num1 * num2
+
+print(result)
+
+# Task 2 - Write code that uses numbers stored in 2 variables and a calculation to output the number 42
