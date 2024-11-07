15971de651682a3ab3601b41de007803fa1785ad
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-07 16:01:31 +0000

    Update 1-1-output.py

--- 1. Python Output, Variables and Input/1-1-output.py
+++ 1. Python Output, Variables and Input/1-1-output.py
@@ -1,28 +1,18 @@
-# Please DON'T REQUEST EDIT ACCESS.
-
-#FORK ME or copy the code! This is the original so it needs to stay undedited for all users.
-
 # Task - Predict and Run
-
 #This is a comment. The computer ignores it. It's just here to give us humans a clue about what the code does.
-
 #Task 1 - Add a comment on the line above the 'print' command to predict what the code on line 8 will do.
-
 
 print("Hello World!")
 print("Hello world")
 
-# Task Investigate
-
+# Task 2 Investigate
 # What would the output of the code print("I love Computing") be?
 print("I love Computing")
 # What would happen if the code print("I love Comping") was run?
 print("I love Comping")
 # What would happen if the code print("I love Computing" was run?
 
-
-# Task Modify
-
+# Task 3 Modify
 # Reuse the code to output your own message. Add comments to show what it does.
 
 
