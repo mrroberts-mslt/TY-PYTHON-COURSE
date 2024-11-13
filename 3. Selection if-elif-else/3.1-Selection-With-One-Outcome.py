d22e9730918d8144a5fb5857d660c78fb5b6adbb
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 18:47:31 +0000

    Create 3.1-Selection-With-One-Outcome.py

---
+++ 3. Selection if-elif-else/3.1-Selection-With-One-Outcome.py
@@ -0,0 +1,22 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1 - Add comments to explain what the input line does, and when the program below will output the text 'This text is output because the condition was true'
+
+num1 = int(input("Enter a number"))
+
+if num1 == 10:
+  print("This text is output because the condition was true")
+
+# Task 2
+
+# Edit the program below so that it works properly for the correct legal driving age.  Add comments to explain what the Boolean operator means.  Change the value in the age variable to
+
+age = 18
+
+if age > 27:
+  print("You are old enough to drive")
+
+
+# Task 3
+
+# Write a program that asks the user to input their name.  If they enter 'Dave' then the program outputs 'Hello Dave'
