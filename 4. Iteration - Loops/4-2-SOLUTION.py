b48127b447dfd040b8eeb38f5f01e1c895d59b0c
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 22:26:36 +0000

    Create 4-2-SOLUTION.py

---
+++ 4. Iteration - Loops/4-2-SOLUTION.py
@@ -0,0 +1,48 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1
+
+  # Add comments to explain what the counter variable is used for, and what +=1 does.
+  # Add a comment that explains what the condition checks for.
+  # Add a line of code inside the loop that outputs the counter variable every time the loop runs.  What do you notice about what happens to it each time the loop repeats?
+  # Add a line of code after the loop that outputs the message 'The loop has finished'
+
+counter = 1
+# Condition checks if the counter variable is less than 5
+while counter < 5:
+  print("This code is inside the loop")
+  #This code adds one to the value in the counter variable
+  counter += 1
+  print (str(counter))
+
+print("The loop has finished")
+
+# Task 2
+
+  # Add a line of code to the loop that subtracts one from the counter every time the loop runs  
+  # Add a line of code after the loop that outputs 'Blast off'
+  # EXTRA CHALLENGE - Adapt the code so that the user inputs the start value of the counter variable.
+
+counter = int(input("Enter a number"))
+
+while counter > 0:
+  print(counter)
+  counter -=1
+
+print("Blast off!")
+
+# Task 3
+
+  # Write a program that uses a loop to output the seven times table up to 12 x 7 (HINT - use the counter variable and combine it with what you learned about maths with variables.)
+  # Output the multiplied number as part of a sentence.  Example - '1 times 7 is 7'
+
+  # EXTRA CHALLENGE - ask the user to input a number and output the multiplication table for the input.
+
+counter = 1
+num1 = int(input("Enter a number"))
+
+while counter <= 12:
+
+  total = num1 * counter
+  print (str(counter) + " times " + str(num1) + " = " + str(total))
+  counter +=1
