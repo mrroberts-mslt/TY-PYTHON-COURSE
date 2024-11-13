ac4a9293377bbe1dcf9725db1d053c4536cb59e3
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 21:51:36 +0000

    Create 2-4-Maths-input.py

---
+++ 2. Maths with Variables/2-4-Maths-input.py
@@ -0,0 +1,13 @@
+# COPY THE CODE
+
+# Task 1 - Add comments to predict what the code will do. Run the code to see if you were correct. 
+
+num1 = input("Enter a number")
+num2 = 10
+
+result = num1 * num2
+
+print(result)
+print()
+
+# Task 2 - edit the code to convert the input to an integer check if the conversion is correct using print(type(result)). 
