27ce526a902a70d772e829830ee6ab8277ba3ac7
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 20:55:03 +0000

    Update 1-4-Input-with-variables.py

--- 1. Python Output, Variables and Input/1-4-Input-with-variables.py
+++ 1. Python Output, Variables and Input/1-4-Input-with-variables.py
@@ -3,7 +3,6 @@
 # Input With Variables
 
 #Task 1 - Add comments to the code below to predict what will happen when it is run.
-
 #Task 2 - add comments to the code to explain what the variables are called and where they are used. Make sure to show where the variable is assigned a value.
 
 print("Hi! What's your name?")
@@ -13,11 +12,11 @@ name = "Dave"
 print("Hi " + name + "! How are you today?")
 
 
+# Task 3 - Adapt the code above so that it asks the user to INPUT their name. 
+# name = input()
+# CHALLENGE - put a prompt in the input command to ask the user for their name. 
 
-# Task 3 - Adapt the code above so that it assigns input into the 'name' variable. CHALLENGE - put a prompt in the input command to ask the user for their name. 
 
-name = input("Hi whats your name")
-print("hello",name)
 # Task 4 - In the code below, combine the lines 'Do you like programming' and 'answer = input', so that it the input has a prompt in it.
 
 #Task 5 - Adapt the output on the next to last line so that it includes both the name and the answer variables.
