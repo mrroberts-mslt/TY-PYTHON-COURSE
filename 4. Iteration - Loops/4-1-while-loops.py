3b38c86858e7ad1ea20f63abb8265fa39100fa3a
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 22:15:59 +0000

    Create 4-1-while-loops.py

---
+++ 4. Iteration - Loops/4-1-while-loops.py
@@ -0,0 +1,31 @@
+# copy the code
+
+# Task 1 - Add comments to explain which code is in the loop and will be repeated.
+
+  # Explain the circumstances in which the loop will run.
+
+answer = input("What is the capital of France?")
+
+while answer != "Paris":
+  print("Incorrect! Try again.")
+  answer = input("What is the capital of France?")
+
+print("Correct!")
+
+
+# Task 2
+
+  # Add a line of code to the loop below that outputs an 'incorrect' message to the user and tells them what to do again.
+  # Add a line of code to the loop below that lets them re-input their number
+  # Add a line of code AFTER the loop below that ouputs a thank you message and repeats the number entered back to the user.  Example - 'Thank you, you typed 7'
+
+num1 = int(input("Enter a number 10 or less"))
+
+while num1 > 10:
+
+
+# Task 3
+
+  # Write a program that stores a secret number in a variable (you decide the number and the name of the variable)
+  # The user has to guess the secret number, the program should loop until they get it right.
+  # Once the user has guessed correctly they get a congratulations message
