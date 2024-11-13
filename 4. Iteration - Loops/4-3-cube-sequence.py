fe6a3ef12b7f62345fcc2a16b50cc644593c9265
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 22:30:49 +0000

    Create 4-3-cube-sequence.py

---
+++ 4. Iteration - Loops/4-3-cube-sequence.py
@@ -0,0 +1,15 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+print("Welcome to the cube sequence generator")
+
+num1 = int(input("How long would you like the sequence to be? Enter a number."))
+
+counter = 1
+
+while counter <= num1:
+  total = counter * counter * counter
+  print(str(total))
+  counter +=1
+
+
+
