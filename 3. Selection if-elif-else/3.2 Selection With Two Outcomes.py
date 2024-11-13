76850e664ef125de81f1bffb89b866a7b4eb11eb
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 18:49:38 +0000

    Create 3.2 Selection With Two Outcomes.py

---
+++ 3. Selection if-elif-else/3.2 Selection With Two Outcomes.py
@@ -0,0 +1,48 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task - Predict and Run
+
+# Add comments to the code to explain:
+  # What will be output when the code is run?
+  # In what circumstances would the other output message be produced
+
+num1 = 42
+
+if num1 == 42:
+  print("You have discovered the meaning of life!")
+else:
+  print("Sorry, you have failed to discover the meaning of life!")
+
+
+# Task Investigate and Modify
+
+# Add to the code below so that it outputs 'You're not Dave!' if the user does not input 'Dave'
+
+name = input("What's your name?")
+
+if name == "Dave":
+  print("Hello Dave")
+
+# What is the condition in the code?
+
+# How would the code work if the operator was !> ?
+
+  #EXTRA CHALLENGE - Adapt the code so that it works in the same way but uses a not equal to Boolean operator.
+
+
+# Task 3 - Make - The login checker
+
+# Write a program that:
+  # Stores the number 1337 in a variable called 'password'
+  # Asks the user to guess the password and stores their input in a new variable (you choose the name)
+  # If the user inputs 1337 then output 'Password correct', otherwise output 'Password incorrect'
+
+
+
+  # Task 4 - Make 2 - Extra Challenge
+
+  # Write a program that:
+    # Asks the user to input two different numbers and stores them in two variables.
+    # Outputs the biggest number entered
+
+    
