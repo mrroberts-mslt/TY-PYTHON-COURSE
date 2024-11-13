4f99c27c0d785e7e824cecd2f8b9b9707765c280
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 21:54:48 +0000

    Create 2-4-SOLUTION.py

---
+++ 2. Maths with Variables/2-4-SOLUTION.py
@@ -0,0 +1,19 @@
+#Gets input and stores in the num1 variable
+num1 = input("Enter a number")
+#Stores 10 in the num2 variable
+num2 = 10
+
+#Replicates the num1 variable 10 times then stores it in the result variable
+result = num1 * num2
+#outputs the result variable
+print(result)
+
+# Task 2 - edit the code to convert the input to an integer. 
+
+num1 = int(input("Enter a number"))
+
+num2 = 10
+
+result = num1 * num2
+
+print(result)
