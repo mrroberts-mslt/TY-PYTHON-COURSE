f40805b4251b736cbfb8edfa8ef7c9c4087cbd7b
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-11-13 20:45:31 +0000

    Update 1-2-Variable-Assignment.py

--- 1. Python Output, Variables and Input/1-2-Variable-Assignment.py
+++ 1. Python Output, Variables and Input/1-2-Variable-Assignment.py
@@ -1,5 +1,4 @@
-# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
-
+# COPY THE CODE TO TRINKET.IO
 #Task 1 - Add comments to the code below to explain what it does
 
 name = "Axl"
@@ -15,10 +14,13 @@ print(name)
 name1 = "Axl"
 name2 = "Slash"
 #Add 2 more variables to store 'Duff' and 'Izzy'
-name3 = "Izzy"
-name4 = "Duff"
+
 
 #This line uses concatenation to join the variables together with the string 'and' to make a sentence.
 
 #Complete the line to output all of the variables 
-print(name1 + " and " + name2 + " and " + " and " + name3 + " and " + name4)
+print(name1 + " and " + name2 + " and " + " and " + name3)
+
+
+
+
