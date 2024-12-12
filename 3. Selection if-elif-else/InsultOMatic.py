fe9c3d3856aaa65c4b2ecb6745b69267ebcf6dad
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-12-12 09:08:16 +0000

    Create InsultOMatic.py

---
+++ 3. Selection if-elif-else/InsultOMatic.py
@@ -0,0 +1,15 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+name = input("Hi, what's your name?")
+num1 = int(input("Pick a number between 1 and 5"))
+
+if num1 == 1:
+  print("Hey " + name + " , I could have coded this in half the time!")
+elif num1 == 2:
+  print("Hey " + name + " , you're a total coding n00b!")
+elif num1 == 3:
+  print("Hey " + name + " , your syntax sucks!")
+elif num1 == 4:
+  print("Hey " + name + " , you'll never master selection! Mwahahahaha!")
+else:
+  print("Hey " + name + " , your code is full of bugs!")
