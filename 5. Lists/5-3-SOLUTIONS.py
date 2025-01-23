eac974fc571107a4ed04b53b47dfa87ba10a7995
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-23 17:32:59 +0000

    Create 5-3-SOLUTIONS.py

---
+++ 5. Lists/5-3-SOLUTIONS.py
@@ -0,0 +1,25 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task 1
+
+food = ["bacon", "cheese", "pasta", "beans"]
+
+# Add comments to explain wht the following lines of code do.
+
+#Adds 'tomatoes' to the end of the list
+food.append("tomatoes")
+
+#Puts 'ice cream' between bacon and cheese
+food.insert(1, "ice cream")
+
+# Removes cheese from the list
+food.remove("cheese")
+
+#Removes the last item (tomatoes) from the list - Students might forget that they have added this earlier so say that it removes beans instead.
+food.pop()
+
+# Add a comment to predict what the list looks like open
+  #bacon, ice cream, pasta, beans
+
+# Write code to print the whole list.  Was your prediction correct?
+print(food)
