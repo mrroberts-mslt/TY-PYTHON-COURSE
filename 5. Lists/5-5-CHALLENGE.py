66e8cd0b9de9bf8b306433554fc9d0b7fb28a092
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-23 17:41:26 +0000

    Create 5-5-CHALLENGE.py

---
+++ 5. Lists/5-5-CHALLENGE.py
@@ -0,0 +1,11 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Task
+
+# Create an array called 'names' that stores five names in it (you choose the names). 
+
+# Ask the user what their name is, store their input in a variable.
+
+# Ask the user to enter a number between 0 and 4. Store their input in a variable.
+
+# Replace the data at the position that matches the number entered by the user in the names array with their name.
