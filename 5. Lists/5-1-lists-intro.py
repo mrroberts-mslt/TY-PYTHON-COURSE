bcc0445a00ea52b4b660b2c9349e51d3f30d6470
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-23 17:00:27 +0000

    Create 5-1-lists-intro.py

---
+++ 5. Lists/5-1-lists-intro.py
@@ -0,0 +1,24 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+fruit = ["Apple", "Banana", "Grape", "Strawberry", "Melon", "Orange"]
+
+# Task 1 - easy
+  # Add comments to predict what the following lines of code will do.
+
+  # Alter the fourth print command so that it outputs a item from the list that hasn't yet been used.
+
+print(fruit[3])
+
+print(fruit[5])
+
+print(fruit[0] + " " + fruit[2])
+
+print(fruit[6])
+
+# Task 2 - easy
+  # Write code to output the whole list - you should be able to do this with one line of code.
+
+
+# Task 3 - moderate to difficult
+  # Ask the user to input a number between 0 and 5 call the variable "usernumber".  
+  # Print the item in the list that matches the number they have input .
