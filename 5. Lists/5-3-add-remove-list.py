14788d6eb102868f5ba3c1154254a63d70fd705d
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-23 17:19:51 +0000

    Create 5-3-add-remove-list.py

---
+++ 5. Lists/5-3-add-remove-list.py
@@ -0,0 +1,19 @@
+
+# Task 1
+
+food = ["bacon", "cheese", "pasta", "beans"]
+
+# Add comments to explain wht the following lines of code do.
+
+food.append("tomatoes")
+
+food.insert(1, "ice cream")
+
+food.remove("cheese")
+
+food.pop()
+
+# Add a comment to predict what the list looks like open
+
+# Write code to print the whole list.  Was your prediction correct?
+
