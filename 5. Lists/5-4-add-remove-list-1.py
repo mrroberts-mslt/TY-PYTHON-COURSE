28ae66da2ecd2c2d624eb7edbe66ef89a6276bf2
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-23 17:36:26 +0000

    Create 5-4-add-remove-list-1.py

---
+++ 5. Lists/5-4-add-remove-list-1.py
@@ -0,0 +1,13 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+videoGames = ["Mario", "Sonic", "Joust", "Zelda"]
+
+#Task 2
+
+# Write code to perform the following tasks.
+
+  # Add 'Minecraft' to the start of the list.
+
+  # Ask the user to input a number between 0 and 4 and store it in a variable.  Output the item at this position in the list.
+
+  # Ask the user to input the name of a video game and store it in a variable.  If this video game is in the list then remove it from the list.  If it isn't in the list then add it to the end.
